6e0d514d2e8a1064aa7ef709bec03416dffc00df
unknown <baudinopierreolivier@gmail.com>
2023-08-28 16:08:03 +0200

    add user reducer

---
+++ src/reducers/user.reducer.js
@@ -0,0 +1,9 @@
+const initialState = { user: "test user" };
+
+export default function userReducer(state = initialState, action) {
+  //switch
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
