f0945ab2690df32e4ffbedf3ae57b406d0df4f26
unknown <baudinopierreolivier@gmail.com>
2023-08-29 11:06:22 +0200

    add update like in bdd

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -4,6 +4,7 @@ export const GET_POSTS = "GET_POSTS";
 export const ADD_POST = "ADD_POST";
 export const EDIT_POST = "EDIT_POST";
 export const DELETE_POST = "DELETE_POST";
+export const ADD_POST_LIKE = "ADD_POST_LIKE";
 
 export const getPosts = () => {
   return (dispatch) => {
@@ -39,5 +40,16 @@ export const deletePost = (postid) => {
       // console.log(res.data)
       dispatch({ type: DELETE_POST, payload: postid });
     });
+  };
+};
+
+export const addPostLike = (data) => {
+  return (dispatch) => {
+    return axios
+      .put(`http://localhost:3000/posts/${data.id}`, data)
+      .then((res) => {
+        // console.log(res.data)
+        dispatch({ type: ADD_POST_LIKE, payload: data });
+      });
   };
 };
